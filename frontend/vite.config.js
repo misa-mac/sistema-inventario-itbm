56ff64ab052707b7ca1208fa60022fe32631155c
misa-mac <raystrom27@gmail.com>
2026-08-31 16:43:53 -0400

    Estructura PWA del fronten

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     react(),
+    basicSsl(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       manifest: {
         name: 'Inventario ITBM',
         short_name: 'ITBM',
